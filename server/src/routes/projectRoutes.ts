4c9f8571d596631faed7fece01ca128072ded90c
Neel-Kunjadiya <neelkunjadiya@gmail.com>
2025-03-12 19:15:41 -0700

    add project route

--- server/src/routes/projectRoutes.ts
+++ server/src/routes/projectRoutes.ts
@@ -4,6 +4,6 @@ import { createProject, getProjects } from "../controllers/projectController";
 const router = Router();
 
 router.get("/", getProjects);
-router.post("/", createProject)
+router.post("/", createProject);
 
 export default router;
